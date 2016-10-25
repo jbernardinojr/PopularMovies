648b6194f6cf31912b30e7afdbc2d008265cc95f
José Bernardino da Silva Júnior <jbernardino.jr@gmail.com>
2016-10-25 17:38:40 -0200

    Adding some urls to connect to the themoviedb service. Changing the name of the folder that contains the logic to connect to the server.

---
+++ app/src/main/java/com/example/jbsjunior/popularmovies/server/URLServer.java
@@ -0,0 +1,13 @@
+package com.example.jbsjunior.popularmovies.server;
+
+/**
+ * Created by JBSJunior on 25/10/2016.
+ */
+
+public class URLServer {
+    public static final String URL_IMAGE_MOVIE = "https://image.tmdb.org/t/p/w185";
+    public static final String URL_API_MOVIE = "https://api.themoviedb.org/3";
+    public static final String API_KEY = "?api_key=3b743993f245d74f2f6a4d68191b19f4";
+    public static final String URL_MOVIE_POPULAR = "/movie/popular";
+    public static final String URL_MOVIE_TOP_RATED = " /movie/top_rated";
+}
